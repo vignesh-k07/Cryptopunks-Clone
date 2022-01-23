0d6454d901bd22e6e5cd81184d7c48bada4f2299
vignesh k <74788727+vignesh-krishnan07@users.noreply.github.com>
2022-01-23 22:47:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
 import { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import PunkList from "./components/PunkList";
 import Main from "./components/Main";
+import Data from "../src/assets/openSeaData.json";
+
+Data.toString();
+console.log(Data["assets"]);
+// const listData = JSON.parse(Data["assets"]);
+// console.log(listData);
 
 function App() {
   const [punkListData, setPunkListData] = useState([]);
@@ -11,10 +17,12 @@ function App() {
 
   useEffect(() => {
     const getmyNfts = async () => {
-      const openSeaData = await axios.get(
-        "/assets?asset_contract_address=0x764357B73F7BF85F59660B1Deb1441730c3ce844&order_direction=desc"
-      );
-      setPunkListData(openSeaData.data.assets);
+      // const openSeaData = await axios.get(
+      //   "/assets?asset_contract_address=0x764357B73F7BF85F59660B1Deb1441730c3ce844&order_direction=desc"
+      // );
+      // setPunkListData(openSeaData.data.assets);
+      // console.log(openSeaData.data.assets);
+      setPunkListData(Data["assets"]);
     };
     return getmyNfts();
   }, []);
